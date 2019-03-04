b6335db0d16f03ab105a75e54863743d0cd1600c
samijv@metropolia.fi <samijv@metropolia.fi>
2019-03-04 20:32:08 +0200

    started authentication

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const JwtStrategy = passportJWT.Strategy;
+const ExtractJwt = passportJWT.ExtractJwt;
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+};
+
+const strategy = new JwtStrategy(opts, (payload, next) => {
+  // TODO: GET USER FROM DB
+  const user = null;
+  next(null, user);
+});
+
+passport.use(strategy);
+app.use(passport.initialize());
+
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
-require('dotenv').config();
 require('./server/server')(io);
 const port = 3000;
 
@@ -28,6 +48,10 @@ app.post('/register', function(req, res) {
 
 app.post('/login', function(req, res) {
 
+});
+
+app.get('/protected', passport.authenticate('jwt', {session: false}), function(req, res) {
+  res.send('im protected');
 });
 
 // Aseta server kuuntelemaan porttia 3000.
