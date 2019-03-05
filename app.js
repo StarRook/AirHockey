6d3b587ab168fa92cbd3c82969e2a863eea1607d
StarRook <samijv@metropolia.fi>
2019-03-05 20:35:54 +0200

    login and register working

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const app = express();
 
-const passport = require('passport');
-const passportJWT = require('passport-jwt');
-const JwtStrategy = passportJWT.Strategy;
-const ExtractJwt = passportJWT.ExtractJwt;
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
-const opts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SECRET
-};
+const bodyParser = require('body-parser');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-const strategy = new JwtStrategy(opts, (payload, next) => {
-  // TODO: GET USER FROM DB
-  const user = null;
-  next(null, user);
-});
-
-passport.use(strategy);
-app.use(passport.initialize());
+// MySQL Connection so we can run queries!
+const connection = require('./db/connection');
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -33,7 +30,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Kun client pyytää get metodilla "/" urlia lähetä sille index.html
 app.get('/', function(req, res){
-  res.render('index');
+  console.log(req.cookies);
+  res.render('index', {
+    page: req.url,
+  });
 });
 
 app.get('/airhockey', function(req, res) {
@@ -43,18 +43,96 @@ app.get('/airhockey', function(req, res) {
 });
 
 app.post('/register', function(req, res) {
+    if (!req.body.username || !req.body.password) {
+      return res.status(401).send('Missing required fields');
+    }
 
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      connection.query(`INSERT INTO users (username, password) VALUES ('${req.body.username}', '${hash}')`, (err, result) => {
+        if (!err) {
+          return res.status(200).send('Account created successfully');
+        } else {
+          return res.status(401).send('MySQL Error' + err);
+        }
+      });
+    });
 });
 
 app.post('/login', function(req, res) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(403).send('Missing required fields');
+  }
+
+  connection.query(`SELECT * FROM users WHERE username = '${req.body.username}'`, (err, result) => {
+    console.log(result);
+    if (err) {
+      return res.status(403).send('No user with this username');
+    }
+    if (result.length > 0) {
+
+      bcrypt.compare(req.body.password, result[0].password, (hashErr, hashResult) => {
+        if (hashErr) {
+          return res.status(403).send('Error white signing in.');
+        }
+        if (hashResult) {
+          const payload = {
+            id: result[0].id,
+            username: result[0].username,
+            expiresIn: '12h'
+          };
+
+          jwt.sign(payload, process.env.JWT_SECRET, {algorithm: 'HS256'}, (jwtErr, token) => {
+            if (jwtErr) {
+              console.log(jwtErr);
+              return res.status(403).send('Error while signing in...');
+            }
+            res.cookie('token', token, {secure: false, httpOnly: true});
+            res.status(200).send({username: result[0].username});
+          });
+        } else {
+          res.status(403).send('Wrong password');
+        }
+      });
+
+    } else {
+      res.status(403).send('No user with this username');
+    }
+  })
 
 });
 
-app.get('/protected', passport.authenticate('jwt', {session: false}), function(req, res) {
-  res.send('im protected');
+const verifyToken = (req, res, next) => {
+  const cookie = req.cookies.token;
+  if (typeof cookie !== "undefined") {
+    req.token = cookie;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+app.post('/logout', verifyToken, (req, res) => {
+  try {
+    jwt.verify(req.token, process.env.JWT_SECRET);
+    res.clearCookie('token');
+    res.status(200).send('OK!');
+  } catch (e) {
+    console.log(e);
+    res.status(403).send(e);
+  }
+});
+
+app.get('/protected', verifyToken, (req, res) => {
+  try {
+    jwt.verify(req.token, process.env.JWT_SECRET);
+    res.status(200).send('OK!');
+  } catch (err) {
+    console.log(err);
+    res.status(403).send('Bad!');
+  }
 });
 
 // Aseta server kuuntelemaan porttia 3000.
-http.listen(process.env.PORT || port, function(){
+http.listen(process.env.PORT || port, () => {
   console.log('listening on *:3000');
 });
---
+++ public/auth.js
@@ -0,0 +1,105 @@
+$(document).ready(function() {
+
+  const loggedindiv = $('#loggedin');
+  const loggedoutdiv = $('#loggedout');
+
+  if (localStorage.getItem('username') !== null && !localStorage.getItem('username').startsWith('Guest')) {
+    console.log('User is logged in');
+    loggedindiv.css('display', 'block');
+    loggedoutdiv.css('display', 'none');
+    $('#welcomemessage').text('Hei, ' + localStorage.getItem('username') + '!');
+  } else {
+    console.log('User is logged out');
+    loggedindiv.css('display', 'none');
+    loggedoutdiv.css('display', 'block');
+    let randomGuestName = "Guest";
+    const randomNumber = Math.floor(Math.random() * 999999) + 100000;
+    randomGuestName = randomGuestName + randomNumber;
+    localStorage.setItem('username', randomGuestName);
+  }
+
+  $("#password, #password_again").on('keyup', (event) => {
+    if ($("#password").val() !== $("#password_again").val()) {
+      $("#registerMessage").text("Salasanat eivät täsmää");
+    } else {
+      $("#registerMessage").text("");
+    }
+  });
+
+  $("#loginButton").on('click', (event) => {
+    login();
+  });
+
+  $("#registerButton").on('click', (event) => {
+    register();
+  });
+
+  $("#logoutButton").on('click', (event) => {
+    logout();
+  });
+
+  function logout() {
+    $.ajax({
+      url: 'http://localhost:3000/logout',
+      type: 'post',
+      success: function(response, textStatus, jqXHR) {
+        localStorage.removeItem('username');
+        window.location.replace('http://localhost:3000');
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert('Uloskirjautuminen epäonnistui');
+      }
+    });
+  }
+
+  function login() {
+
+    const serializedData = $('#loginForm').serialize();
+    console.log(serializedData);
+
+    $.ajax({
+      url: 'http://localhost:3000/login',
+      type: 'post',
+      data: serializedData,
+      success: function(response, textStatus, jqXHR) {
+        localStorage.setItem('username', response.username);
+        loggedindiv.css('display', 'block');
+        loggedoutdiv.css('display', 'none');
+        $('#closeLoginModal').click();
+        $('#welcomemessage').text('Hei, ' + localStorage.getItem('username') + '!');
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error(
+          "The following error occurred: "+
+          textStatus, errorThrown
+        );
+      }
+    });
+
+  }
+
+  function register() {
+    if ($("#password").val() === $("#password_again").val()) {
+      const allowed = /^[0-9a-zA-Z]+$/;
+      if ($("#username").val().match(allowed)) {
+        const serializedData = $('#registerForm').serialize();
+        console.log(serializedData);
+        $.ajax({
+          url: 'http://localhost:3000/register',
+          type: 'post',
+          data: serializedData,
+          success: function(response, textStatus, jqXHR) {
+            $('#closeRegisterModal').click();
+            $('#openLoginModalButton').click();
+            $('#loginMessage').text('Rekisteröityminen onnistui! Voit nyt kirjautua sisään!');
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            console.log('The following error occurred: '+textStatus,errorThrown);
+          }
+        });
+      } else {
+        alert('Käyttäjänimi voi sisältää vain kirjaimia ja numeroita');
+      }
+    }
+  }
+});
